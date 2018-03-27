bfd1565afe4055c07f134c429fdbaf19a7ec0efd
Florian Keller <github@floriankeller.de>
2018-03-27 12:14:10 +0200

    Add ocsp

--- types/modules/KJUR/asn1/index.d.ts
+++ types/modules/KJUR/asn1/index.d.ts
@@ -3,6 +3,7 @@
 /// <reference path="./ASNObjects.d.ts" />
 /// <reference path="./cades/index.d.ts" />
 /// <reference path="./cms/index.d.ts" />
+/// <reference path="./ocsp/index.d.ts" />
 
 declare namespace jsrsasign.KJUR {
   namespace asn1 {
---
+++ types/modules/KJUR/asn1/ocsp/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="./CertID.d.ts" />
+/// <reference path="./Request.d.ts" />
+/// <reference path="./TBSRequest.d.ts" />
+/// <reference path="./OCSPRequest.d.ts" />
+/// <reference path="./OCSPUtil.d.ts" />
+
+declare namespace jsrsasign.KJUR.asn1 {
+  namespace ocsp {
+    //
+  }
+}
